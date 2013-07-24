a740aab787c9486285064d595a19ae8aea1efde8
doom119 <zhangran119@gmail.com>
2013-07-24 22:53:49 +0800

    multify glsl

--- Test1/assets/fragment_shader_1.glsl
+++ Test1/assets/fragment_shader_1.glsl
@@ -1,5 +1,7 @@
 varying vec4 v_color;
+uniform sampler2D u_texture;
+varying vec2 v_texCoordinate;
 void main()
 {
-	gl_FragColor = v_color;
+	gl_FragColor = v_color * texture2D(u_texture, v_texCoordinate);
 }
--- Test1/assets/vertex_shader_1.glsl
+++ Test1/assets/vertex_shader_1.glsl
@@ -1,8 +1,11 @@
 attribute vec3 a_position;
 attribute vec4 a_color;
 varying vec4 v_color;
+attribute vec2 a_texCoordinate;
+varying vec2 v_texCoordinate;
 void main()
 {
 	v_color = a_color;
+	v_texCoordinate = a_texCoordinate;
 	gl_Position = vec4(a_position, 1.0);
 }
